fe33b618f7a30231c25d5fac271d6cdacc6e58d2
adamgabry <adam.gabrys@seznam.cz>
2023-12-14 17:14:15 +0100

    centered

--- lib/src/view/car_home_help_screen.dart
+++ lib/src/view/car_home_help_screen.dart
@@ -23,103 +23,105 @@ class _HelpCallPageState extends State<HelpCallPage> {
       appBar: AppBar(
         title: Text('Emergency Call'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 100.0),
-            Text(
-              'In the event of an accident\ncall assistance service:',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 26.0,
-                fontWeight: FontWeight.bold,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 100.0),
+              Text(
+                'In the event of an accident\ncall assistance service:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 70.0),
-            // Adjust this value as needed for spacing
-            Text(
-              'NONSTOP',
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
+              SizedBox(height: 70.0),
+              // Adjust this value as needed for spacing
+              Text(
+                'NONSTOP',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 70.0),
-            // Adjust this value as needed for spacing
-            Text(
-              'Number: 1234567890', // Replace with the actual number
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w500,
+              SizedBox(height: 70.0),
+              // Adjust this value as needed for spacing
+              Text(
+                'Number: 1234567890', // Replace with the actual number
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            SizedBox(height: 40.0),
-            // Reduce this value to bring the slider closer to the number
-            GestureDetector(
-              onHorizontalDragUpdate: (details) {
-                // Update the slider value only if the drag is to the right
-                if (details.primaryDelta! > 0) {
-                  double newSlideValue =
-                      _slideValue + details.primaryDelta! / _sliderWidth;
-                  newSlideValue = newSlideValue.clamp(0.0, 1.0);
+              SizedBox(height: 40.0),
+              // Reduce this value to bring the slider closer to the number
+              GestureDetector(
+                onHorizontalDragUpdate: (details) {
+                  // Update the slider value only if the drag is to the right
+                  if (details.primaryDelta! > 0) {
+                    double newSlideValue =
+                        _slideValue + details.primaryDelta! / _sliderWidth;
+                    newSlideValue = newSlideValue.clamp(0.0, 1.0);
 
-                  setState(() {
-                    _slideValue = newSlideValue;
-                  });
+                    setState(() {
+                      _slideValue = newSlideValue;
+                    });
 
-                  // If the slider reaches the end, make the call
-                  if (_slideValue == 1.0) {
-                    _makePhoneCall(
-                        '1234567890'); // Replace with your phone number
+                    // If the slider reaches the end, make the call
+                    if (_slideValue == 1.0) {
+                      _makePhoneCall(
+                          '1234567890'); // Replace with your phone number
+                    }
                   }
-                }
-              },
-              onHorizontalDragEnd: (details) {
-                // Reset the slider when user stops dragging
-                setState(() {
-                  _slideValue = 0.0;
-                });
-              },
-              child: Container(
-                height: _sliderHeight,
-                width: _sliderWidth,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade300,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Stack(
-                  alignment: Alignment.centerLeft,
-                  children: [
-                    Container(
-                      width: _slideValue * _sliderWidth,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(30.0),
+                },
+                onHorizontalDragEnd: (details) {
+                  // Reset the slider when user stops dragging
+                  setState(() {
+                    _slideValue = 0.0;
+                  });
+                },
+                child: Container(
+                  height: _sliderHeight,
+                  width: _sliderWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [
+                      Container(
+                        width: _slideValue * _sliderWidth,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
                       ),
-                    ),
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Icon(Icons.arrow_forward_ios),
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Icon(Icons.arrow_forward_ios),
+                        ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Opacity(
-                        opacity: (1 - _slideValue).clamp(0.0, 1.0),
-                        child: Text('slide to call'),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Opacity(
+                          opacity: (1 - _slideValue).clamp(0.0, 1.0),
+                          child: Text('slide to call'),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
